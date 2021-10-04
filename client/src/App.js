01f183f81c0676ff462431e96ad5c3647df43bcb
Edoumou <devforsay@gmail.com>
2021-10-04 17:10:04 +0200

    cleaning up files

--- client/src/App.js
+++ client/src/App.js
@@ -39,24 +39,13 @@ class App extends Component {
       //===================================================
     } catch (error) {
       alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
+        `Failed to load web3, accounts, or contract..`
       );
       console.error(error);
     }
   };
 
   runExample = async () => {
-    const { account, IPBCcontract } = this.state;
-    console.log("Contract:", IPBCcontract);
-
-    // Stores a given value, 5 by default.
-    await IPBCcontract.methods.set(5).send({ from: account });
-
-    // Get the value from the contract to prove it worked.
-    const response = await IPBCcontract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
   };
 
   render() {
@@ -66,16 +55,6 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
   }
--- client/src/IPBCSignatureContract.js
+++ client/src/IPBCSignatureContract.js
@@ -4,8 +4,6 @@ const IPBCSignatureContract = async (web3) => {
     const networkId = await web3.eth.net.getId();
     const deployedNetwork = IPBCSignature.networks[networkId];
 
-    console.log("Contract address:", deployedNetwork.address);
-
     return new web3.eth.Contract(
         IPBCSignature.abi,
         deployedNetwork && deployedNetwork.address
